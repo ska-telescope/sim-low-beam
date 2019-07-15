72ee7d98f85e0c4cd50741816f01767d073a1ce4
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2019-07-15 14:51:25 +0100

    Added bright sources to original simulations.

--- beam_errors/run_sims.py
+++ beam_errors/run_sims.py
@@ -3,13 +3,12 @@
 """Script to run LOW station beam error simulations."""
 
 from __future__ import print_function, division
+import json
 import logging
 import os
-import shutil
 import sys
-import tempfile
 
-import json
+from astropy.time import Time, TimeDelta
 import matplotlib
 matplotlib.use('Agg')
 # pylint: disable=wrong-import-position
@@ -21,20 +20,42 @@ import oskar
 LOG = logging.getLogger()
 
 
+def append_bright_sources(sky):
+    """Appends bright A-team sources to sky model."""
+    # A list of bright sources.
+    # Sgr A: guesstimates only!
+    # For A: data from the Molonglo Southern 4 Jy sample (VizieR).
+    # Others from GLEAM reference paper, Hurley-Walker et al. (2017), Table 2.
+    # pylint: disable=bad-whitespace
+    sky_bright = oskar.Sky.from_array(numpy.array((
+        [266.41683, -29.00781,  2000,0,0,0,   0,    0,    0, 3600, 3600, 0],
+        [ 50.67375, -37.20833,   528,0,0,0, 178e6, -0.51, 0, 0, 0, 0],  # For
+        [201.36667, -43.01917,  1370,0,0,0, 200e6, -0.50, 0, 0, 0, 0],  # Cen
+        [139.52500, -12.09556,   280,0,0,0, 200e6, -0.96, 0, 0, 0, 0],  # Hyd
+        [ 79.95833, -45.77889,   390,0,0,0, 200e6, -0.99, 0, 0, 0, 0],  # Pic
+        [252.78333,   4.99250,   377,0,0,0, 200e6, -1.07, 0, 0, 0, 0],  # Her
+        [187.70417,  12.39111,   861,0,0,0, 200e6, -0.86, 0, 0, 0, 0],  # Vir
+        [ 83.63333,  22.01444,  1340,0,0,0, 200e6, -0.22, 0, 0, 0, 0],  # Tau
+        [299.86667,  40.73389,  7920,0,0,0, 200e6, -0.78, 0, 0, 0, 0],  # Cyg
+        [350.86667,  58.81167, 11900,0,0,0, 200e6, -0.41, 0, 0, 0, 0]   # Cas
+        )))
+    sky.append(sky_bright)
+
+
+def get_start_time(ra0_deg, length_sec):
+    """Returns optimal start time for field RA and observation length."""
+    t = Time('2000-01-01 00:00:00', scale='utc', location=('116.764d', '0d'))
+    dt_hours = 24.0 - t.sidereal_time('apparent').hour + (ra0_deg / 15.0)
+    start = t + TimeDelta(dt_hours * 3600.0 - length_sec / 2.0, format='sec')
+    return start.value
+
+
 def make_vis_data(settings, sky, tel):
     """Run simulation using supplied settings."""
-    out_ms = settings['interferometer/ms_filename']
-    out_vis = settings['interferometer/oskar_vis_filename']
-    out_files = []
-    if out_ms and not os.path.exists(out_ms):
-        out_files.append(out_ms)
-    if out_vis and not os.path.exists(out_vis):
-        out_files.append(out_vis)
-    if not out_files:
+    if os.path.exists(settings['interferometer/oskar_vis_filename']):
         LOG.info("Skipping simulation, as output data already exist.")
         return
-
-    LOG.info("Simulating %s", ', '.join(out_files))
+    LOG.info("Simulating %s", settings['interferometer/oskar_vis_filename'])
     if sky.num_sources == 1:
         settings['simulator/use_gpus'] = 'false'
         settings['simulator/max_sources_per_chunk'] = '2'
@@ -45,7 +66,8 @@ def make_vis_data(settings, sky, tel):
     sim.run()
 
 
-def make_diff_image_stats(filename1, filename2, out_image_root=None):
+def make_diff_image_stats(filename1, filename2, use_w_projection,
+                          out_image_root=None):
     """Make an image of the difference between two visibility data sets.
 
     This function assumes that the observation parameters for both data sets
@@ -58,47 +80,28 @@ def make_diff_image_stats(filename1, filename2, out_image_root=None):
         imager.output_root = out_image_root
 
     LOG.info("Imaging differences between '%s' and '%s'", filename1, filename2)
-    if filename1.lower().endswith('.ms') and filename2.lower().endswith('.ms'):
-        handle1 = oskar.MeasurementSet.open(filename1)
-        handle2 = oskar.MeasurementSet.open(filename2)
-        num_rows = handle1.num_rows
-        num_channels = handle1.num_channels
-        num_stations = handle1.num_stations
-        if handle2.num_rows != num_rows or \
-                handle2.num_channels != num_channels or \
-                handle2.num_stations != num_stations:
-            raise RuntimeError("'%s' and '%s' have different dimensions!" %
-                               (filename1, filename2))
-        num_baselines = num_stations * (num_stations - 1) // 2
-        num_chunks = (num_rows + num_baselines - 1) // num_baselines
-        imager.set_vis_frequency(handle1.freq_start_hz,
-                                 handle1.freq_inc_hz, num_channels)
-        for i_chunk in range(num_chunks):
-            start_row = i_chunk * num_baselines
-            (uu, vv, ww) = handle1.read_coords(start_row, num_baselines)
-            vis1 = handle1.read_vis(start_row, 0, num_channels, num_baselines)
-            vis2 = handle2.read_vis(start_row, 0, num_channels, num_baselines)
-            vis_out = vis1 - vis2
-            imager.update(uu, vv, ww, amps=vis_out,
-                          start_channel=0, end_channel=num_channels-1)
-        del handle1, handle2
-    else:
-        (hdr1, handle1) = oskar.VisHeader.read(filename1)
-        (hdr2, handle2) = oskar.VisHeader.read(filename2)
-        block1 = oskar.VisBlock.create_from_header(hdr1)
-        block2 = oskar.VisBlock.create_from_header(hdr2)
-        if hdr1.num_blocks != hdr2.num_blocks or \
-                hdr1.num_stations != hdr2.num_stations or \
-                hdr1.max_times_per_block != hdr2.max_times_per_block or \
-                hdr1.max_channels_per_block != hdr2.max_channels_per_block:
-            raise RuntimeError("'%s' and '%s' have different dimensions!" %
-                               (filename1, filename2))
+    (hdr1, handle1) = oskar.VisHeader.read(filename1)
+    (hdr2, handle2) = oskar.VisHeader.read(filename2)
+    block1 = oskar.VisBlock.create_from_header(hdr1)
+    block2 = oskar.VisBlock.create_from_header(hdr2)
+    if hdr1.num_blocks != hdr2.num_blocks:
+        raise RuntimeError("'%s' and '%s' have different dimensions!" %
+                           (filename1, filename2))
+    if use_w_projection:
+        imager.set(algorithm='W-projection')
+        imager.coords_only = True
         for i_block in range(hdr1.num_blocks):
             block1.read(hdr1, handle1, i_block)
-            block2.read(hdr2, handle2, i_block)
-            block1.cross_correlations()[...] -= block2.cross_correlations()
             imager.update_from_block(hdr1, block1)
-        del handle1, handle2, hdr1, hdr2, block1, block2
+        imager.coords_only = False
+        imager.check_init()
+        LOG.info("Using %d W-planes", imager.num_w_planes)
+    for i_block in range(hdr1.num_blocks):
+        block1.read(hdr1, handle1, i_block)
+        block2.read(hdr2, handle2, i_block)
+        block1.cross_correlations()[...] -= block2.cross_correlations()
+        imager.update_from_block(hdr1, block1)
+    del handle1, handle2, hdr1, hdr2, block1, block2
 
     # Finalise image and return it to Python.
     output = imager.finalise(return_images=1)
@@ -111,13 +114,10 @@ def make_diff_image_stats(filename1, filename2, out_image_root=None):
         (image_size - box_size)//2:(image_size + box_size)//2,
         (image_size - box_size)//2:(image_size + box_size)//2]
     return {
-        'image_max': numpy.max(image),
-        'image_min': numpy.min(image),
-        'image_maxabs': numpy.max(numpy.abs(image)),
         'image_medianabs': numpy.median(numpy.abs(image)),
         'image_mean': numpy.mean(image),
+        'image_std': numpy.std(image),
         'image_rms': numpy.sqrt(numpy.mean(image**2)),
-        'image_centre_maxabs': numpy.max(numpy.abs(centre)),
         'image_centre_mean': numpy.mean(centre),
         'image_centre_std': numpy.std(centre),
         'image_centre_rms': numpy.sqrt(numpy.mean(centre**2))
@@ -178,11 +178,9 @@ def make_plot(prefix, field_name, metric_key, results,
     metric_name = '[ UNKNOWN ]'
     if metric_key == 'image_centre_rms':
         metric_name = 'Central RMS [Jy/beam]'
-    elif metric_key == 'image_maxabs':
-        metric_name = 'MAX(ABS(image)) [Jy/beam]'
     elif metric_key == 'image_medianabs':
         metric_name = 'MEDIAN(ABS(image)) [Jy/beam]'
-    sky_model = 'GLEAM'
+    sky_model = 'GLEAM + A-team'
     if single_source_offset >= 0:
         sky_model = 'single source'
     plt.title('%s for %s field (%s)' % (metric_name, field_name, sky_model))
@@ -210,13 +208,12 @@ def run_single(prefix_field, settings, sky, tel,
     out_image_root = None
     if out == 'GLEAM_100_MHz_EoR0_0.170_dB_1.20_deg':
         out_image_root = out
-    results[out] = make_diff_image_stats(out0_name, out_name, out_image_root)
-
-    # Delete output visibility data to save space.
-    if os.path.isdir(out_name):
-        shutil.rmtree(out_name)
-    else:
-        os.remove(out_name)
+    use_w_projection = True
+    if sky.num_sources == 1:
+        use_w_projection = False
+    results[out] = make_diff_image_stats(out0_name, out_name, use_w_projection,
+                                         out_image_root)
+    #os.remove(out_name)  # Delete visibility data to save space.
 
 
 def run_set(prefix, base_settings, fields, axis_gain, axis_phase, specials,
@@ -245,19 +242,22 @@ def run_set(prefix, base_settings, fields, axis_gain, axis_phase, specials,
             settings.from_dict(settings_dict)
             ra_deg = float(settings['observation/phase_centre_ra_deg'])
             dec_deg = float(settings['observation/phase_centre_dec_deg'])
+            length_sec = float(settings['observation/length'])
+            settings['observation/start_time_utc'] = get_start_time(
+                ra_deg, length_sec)
             tel.set_phase_centre(ra_deg, dec_deg)
 
-            # Load the sky model.
+            # Load or create the sky model.
             if single_source_offset >= 0:
-                (_, temp_sky) = tempfile.mkstemp(suffix='.osm')
-                with open(temp_sky, 'w') as fhan:
-                    fhan.write("%.5f %.5f 1\n" %
-                               (ra_deg, dec_deg - single_source_offset))
-                settings['sky/oskar_sky_model/file'] = temp_sky
-                sky = oskar.Sky(settings=settings)
-                os.remove(temp_sky)
+                sky = oskar.Sky.from_array(numpy.array((
+                    [ra_deg, dec_deg - single_source_offset, 1])))
+                settings['interferometer/ignore_w_components'] = 'true'
             else:
                 sky = oskar.Sky(settings=settings)
+                num_sources0 = sky.num_sources
+                append_bright_sources(sky)
+                assert sky.num_sources - num_sources0 == 10
+                settings['interferometer/ignore_w_components'] = 'false'
 
             # Simulate the 'perfect' case.
             tel.override_element_gains(1.0, 0.0)
@@ -284,8 +284,6 @@ def run_set(prefix, base_settings, fields, axis_gain, axis_phase, specials,
             make_plot(prefix, field_name, 'image_centre_rms', results,
                       axis_gain, axis_phase, specials, single_source_offset)
         else:
-            make_plot(prefix, field_name, 'image_maxabs', results,
-                      axis_gain, axis_phase, specials, single_source_offset)
             make_plot(prefix, field_name, 'image_medianabs', results,
                       axis_gain, axis_phase, specials, single_source_offset)
 
@@ -307,12 +305,12 @@ def main():
         'simulator': {
             'double_precision': 'true',
             'use_gpus': 'true',
-            'max_sources_per_chunk': '20000'
+            'max_sources_per_chunk': '23000'
         },
         'observation' : {
             'start_frequency_hz': '100e6',
             'frequency_inc_hz': '100e3',
-            'length': '06:00:00',
+            'length': '21600.0',
             'num_time_steps': '36'
         },
         'telescope': {
@@ -323,8 +321,7 @@ def main():
         'interferometer': {
             'channel_bandwidth_hz': '100e3',
             'time_average_sec': '1.0',
-            'max_time_samples_per_block': '4',
-            'ignore_w_components': 'true'
+            'max_time_samples_per_block': '4'
         }
     }
 
@@ -333,20 +330,17 @@ def main():
         'EoR0': {
             'sky/oskar_sky_model/file': 'EoR0_20deg_point_source.osm',
             'observation/phase_centre_ra_deg': '0.0',
-            'observation/phase_centre_dec_deg': '-27.0',
-            'observation/start_time_utc': '2000-01-01 07:00:00.0'
+            'observation/phase_centre_dec_deg': '-27.0'
         },
         'EoR1': {
             'sky/oskar_sky_model/file': 'EoR1_20deg_point_source.osm',
             'observation/phase_centre_ra_deg': '60.0',
-            'observation/phase_centre_dec_deg': '-30.0',
-            'observation/start_time_utc': '2000-01-01 11:00:00.0'
+            'observation/phase_centre_dec_deg': '-30.0'
         },
         'EoR2': {
             'sky/oskar_sky_model/file': 'EoR2_20deg_point_source.osm',
             'observation/phase_centre_ra_deg': '170.0',
-            'observation/phase_centre_dec_deg': '-10.0',
-            'observation/start_time_utc': '2000-01-01 17:45:00.0'
+            'observation/phase_centre_dec_deg': '-10.0'
         }
     }
     axis_gain = [0.005, 0.01, 0.02, 0.04, 0.08, 0.16, 0.32, 0.64]
@@ -373,7 +367,7 @@ def main():
             fields, axis_gain, axis_phase, specials, single_source_offset,
             plot_only)
 
-    # GLEAM sky model simulations.
+    # GLEAM + A-team sky model simulations.
     run_set('GLEAM_100_MHz', base_settings,
             fields, axis_gain, axis_phase, specials, -1, plot_only)
 
